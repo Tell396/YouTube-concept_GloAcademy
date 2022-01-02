45aa3f7bca43a3888e20d344ec1c4e07c609a31b
Tell <55622895+Tell396@users.noreply.github.com>
2022-01-02 15:09:46 +1000

    Added responsive

--- script/main.js
+++ script/main.js
@@ -1,10 +1,28 @@
 const channel = () => {
   let swiper = new Swiper('.channel-slider', {
-    // Optional parameters
+    
     loop: true,
-    slidesPerView: 6,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1900: {
+        slidesPerView: 6
+      },
+      1600: {
+        slidesPerView: 5
+      },
+      1300: {
+        slidesPerView: 4
+      },
+      992: {
+        slidesPerView: 3
+      },
+      800: {
+        slidesPerView: 2
+      }
+    },
 
-    // Navigation arrows
+    
     navigation: {
       nextEl: '.channel-button-next',
       prevEl: '.channel-button-prev',
@@ -14,11 +32,18 @@ const channel = () => {
 
 const recommended = () => {
   let swiper = new Swiper('.recommended-slider', {
-    // Optional parameters
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      1600: {
+        slidesPerView: 3
+      },
+      1100: {
+        slidesPerView: 2
+      }
+    },
 
-    // Navigation arrows
     navigation: {
       nextEl: '.channel-recommended-button-next',
       prevEl: '.channel-recommended-button-prev',
@@ -28,11 +53,10 @@ const recommended = () => {
 
 const food = () => {
   let swiper = new Swiper('.food-slider', {
-    // Optional parameters
     loop: true,
     slidesPerView: 6,
 
-    // Navigation arrows
+    
     navigation: {
       nextEl: '.channel-food-recommended-button-next',
       prevEl: '.channel-food-recommended-button-prev',
@@ -43,3 +67,29 @@ const food = () => {
 channel();
 recommended();
 food();
+
+
+const searchBtn = document.querySelector('.mobile-search');
+
+  
+const mobileSearch = () => {
+  const mobileSearchGroup = document.querySelector('.input-group')
+  let isOpen = false
+
+  document.addEventListener('click', (e) => {
+    if (e.target.closest('.mobile-search')) {
+      mobileSearchGroup.classList.add('is-open')
+      isOpen = true
+    } else if (!e.target.closest('.input-group') && isOpen) {
+      mobileSearchGroup.classList.remove('is-open')
+      isOpen = false
+    }
+  })
+}
+
+
+if (document.documentElement.scrollWidth <= 640) {
+  swiperMyChannel.destroy()
+  swiperRecommended.destroy()
+  swiperFoodDrink.destroy()
+}
